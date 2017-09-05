f85617bb7295b9a921b2bd8df689fc55925fad91
Jcardif <josh.cardif14@outlook.com>
2017-09-05 11:44:51 +0300

    Intialise solution

---
+++ FbaseAuth/FbaseAuth/Activities/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Firebase;
+using Firebase.Auth;
+
+namespace FbaseAuth
+{
+    [Activity(Label = "FbaseAuth", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnLogin;
+        EditText email_input, pass_input;
+        TextView txt_signUp, txt_forgotPass;
+
+        RelativeLayout relativeMain;
+
+        public static FirebaseApp app;
+        FirebaseAuth auth;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
