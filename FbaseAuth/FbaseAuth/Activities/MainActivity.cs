673d1e8bd8279aa69d26b2338e270afcd2155682
Jcardif <josh.cardif14@outlook.com>
2017-09-05 13:18:04 +0300

    Init firebase

--- FbaseAuth/FbaseAuth/Activities/MainActivity.cs
+++ FbaseAuth/FbaseAuth/Activities/MainActivity.cs
@@ -4,11 +4,16 @@ using Android.OS;
 using Android.Support.V7.App;
 using Firebase;
 using Firebase.Auth;
+using System;
+using static Android.Views.View;
+using Android.Views;
+using Android.Gms.Tasks;
+using Android.Support.Design.Widget;
 
 namespace FbaseAuth
 {
-    [Activity(Label = "FbaseAuth", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "FbaseAuth", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/Firebase")]
+    public class MainActivity : AppCompatActivity, IOnClickListener, IOnCompleteListener
     {
         Button btnLogin;
         EditText email_input, pass_input;
@@ -24,7 +29,72 @@ namespace FbaseAuth
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            //init firebase
+            InitFirebaseAuth();
+
+            //view
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            email_input = FindViewById<EditText>(Resource.Id.loginEmail);
+            pass_input = FindViewById<EditText>(Resource.Id.loginPassword);
+            txt_signUp = FindViewById<TextView>(Resource.Id.signUp);
+            txt_forgotPass = FindViewById<TextView>(Resource.Id.forgotpass);
+            relativeMain = FindViewById<RelativeLayout>(Resource.Id.relativeMain);
+
+            txt_signUp.SetOnClickListener(this);
+            txt_forgotPass.SetOnClickListener(this);
+            btnLogin.SetOnClickListener(this);
+        }
+
+        private void InitFirebaseAuth()
+        {
+            var options = new FirebaseOptions.Builder()
+                .SetApplicationId("1:249232069396:android:cbe0f415e0307455")
+                .SetApiKey("AIzaSyBoPHjd6w92ZDXFJ4YiVlZEFt0N4_tsXDQ")
+                .Build();
+
+            if (app == null)
+                app = FirebaseApp.InitializeApp(this, options);
+                    auth = FirebaseAuth .GetInstance(app);
+        }
+
+        public void OnClick(View v)
+        {
+           if (v.Id == Resource.Id.forgotpass)
+            {
+                StartActivity(new Android.Content.Intent(this, (typeof(ForgotPass))));
+                Finish();
+            }
+           else if (v.Id == Resource.Id.signUp)
+            {
+                StartActivity(new Android.Content.Intent(this, (typeof(SignUp))));
+                Finish();
+            }
+            else if (v.Id == Resource.Id.btnLogin)
+            {
+                LoginUser(email_input.Text, pass_input.Text);
+            }
+        }
+
+        private void LoginUser(string email, string password)
+        {
+            auth.SignInWithEmailAndPassword(email, password)
+                .AddOnCompleteListener(this);
+        }
+
+        public void OnComplete(Task task)
+        {
+            if (task.IsSuccessful)
+            {
+                StartActivity(new Android.Content.Intent(this, (typeof(Dashboard))));
+                Finish();
+            }
+            else
+            {
+                Snackbar sbarMain = Snackbar.Make(relativeMain, "Log In Failed", Snackbar.LengthShort);
+                sbarMain.Show();
+            }
         }
     }
 }
